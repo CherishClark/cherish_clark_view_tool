e1c7e51a23e544fd3abeb4c76ca16d6c41fa74fa
Cherish Clark <cherishclark@Cherishs-MBP.ad.uvu.edu>
2017-12-12 09:54:55 -0700

    Dev tool initial commit

---
+++ lib/cherish_clark_view_tool.rb
@@ -0,0 +1,6 @@
+require "cherish_clark_view_tool/version"
+require "cherish_clark_view_tool/renderer"
+
+module CherishClarkViewTool
+  # Your code goes here...
+end
---
+++ lib/cherish_clark_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CherishClarkViewTool
+  VERSION = "0.1.0"
+end
